6703f374a949b31332fc1d9325e3f1b91dc3c497
Kenny Weigel <kennyweigel@gmail.com>
2013-03-02 21:20:12 -0500

    added style sheets

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -14,21 +14,11 @@ var app = {
     },
 */
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-    
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 
     showAlert: function (message, title) {
@@ -40,10 +30,6 @@ var app = {
         }
     },
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
 };
 
 
