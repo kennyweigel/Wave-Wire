ba2983ae19a0c9d47c9762e6aba7288239e230fc
Kenny Weigel <kennyweigel@gmail.com>
2013-03-02 20:47:14 -0500

    added handlebars to help with html markup

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,5 +1,5 @@
 var app = {
-
+/*
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -12,13 +12,23 @@ var app = {
             }
         });
     },
+*/
 
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+    
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
             self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 
     showAlert: function (message, title) {
@@ -31,13 +41,7 @@ var app = {
     },
 
     renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 };
