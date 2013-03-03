2f933967f3bb86ec5d94087cd8330d69c27fb3de
Kenny Weigel <kennyweigel@gmail.com>
2013-03-03 17:34:31 -0500

    fixed view routing and changed to local storage store

--- phonegap-workshop-master/js/EmployeeView.js
+++ phonegap-workshop-master/js/EmployeeView.js
@@ -1,18 +1,17 @@
-var EmployeeView = function() {
+
+
+var EmployeeView = function(employee) {
 	
 	this.initialize = function() {
 		this.el = $('<div/>');
 	};
-
-	this.initialize();
 
 	this.render = function() {
 	    this.el.html(EmployeeView.template(employee));
 	    return this;
 	};
 
-
-
+	this.initialize();
 }
 
-EmployeeView.template = Handlebars.complile($('#employee-tpl').html());
+EmployeeView.template = Handlebars.compile($('#employee-tpl').html());
--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,38 +1,8 @@
+
 var app = {
-/*
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-*/
-
-    initialize: function() {
-        var self = this;
-        this.detailsURL = /^#employees\/(\d{1,})/;
-        this.registerEvents();
-        this.store = new MemoryStore(function() {
-            self.route();
-        });
-    },
-
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } 
-        else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
 
     registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
         var self = this;
         // Check of browser supports touch events...
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
@@ -53,7 +23,7 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
-        $(window).on('hashchange', $.proxy(this.route, this));
+        
     },
 
     route: function() {
@@ -69,8 +39,26 @@ var app = {
             });
         }
     },
+
+    initialize: function() {
+        var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
+        this.store = new LocalStorageStore(function() {
+            self.route();
+        });
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } 
+        else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    }
 };
 
-
-
 app.initialize();
+
+
