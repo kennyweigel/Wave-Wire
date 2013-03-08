a5cb4b8c67bf4fea8aaa0c040b266a5a1576f16f
Kenny Weigel <kennyweigel@gmail.com>
2013-03-07 20:38:30 -0500

    added accordian and fixed event handlers

--- www/js/MapView.js
+++ www/js/MapView.js
@@ -1,58 +1,59 @@
 
 
 var MapView = function() {
-	
-	this.initialize = function() {
-		this.el = $('<div/>');
-		//this.el.on('click','#mapBtn', this.buoyMap);
-    this.el.on('click','#mapRefresh', this.buoyMap);
-		//this.el.on('click', '.add-location-btn', this.addLocation);
-		//this.el.on('click', '.add-contact-btn', this.addToContacts);
-		//this.el.on('click', '.change-pic-btn', this.changePicture);
-	};
+  
+  this.initialize = function() {
+    this.el = $('<div/>');
+      this.registerEvents();
 
-	this.render = function() {
-	    this.el.html(MapView.template());
-	    return this;
-	};
+  };
 
-	this.buoyMap = function(event) {
-	    navigator.geolocation.getCurrentPosition(onSuccess, onError,{'enableHighAccuracy':true,'timeout':10000});
-	}
+  this.registerEvents = function() {
+    this.el.on('click','#mapRefresh',this.buoyMap);
+  }
 
-	//onSuccess callback receives a Position object
-	var onSuccess = function(position) {
-	    
-	    var myLat = position.coords.latitude;
-	    var myLng = position.coords.longitude;
-	    var myLatLng = new google.maps.LatLng(myLat,myLng)
-	    
-	    //MAP
-	    var mapOptions = {
-	        center: myLatLng,
-	        zoom: 7,
-	        mapTypeId: google.maps.MapTypeId.TERRAIN,
-	        disableDefaultUI: true
-	    };
-      
-      //var navHeight = $('.navbar').height();
-      //$('#mapCanvas').css({ top: navHeight });	    
-      
-      var map = new google.maps.Map(document.getElementById("mapCanvas"),mapOptions);
+  this.render = function() {
+      this.el.html(MapView.template());
+      return this;
+  };
 
-	    var marker = new google.maps.Marker({
-	        position: myLatLng,
-	        map: map,
-	        title: 'Me'
-	    });
-	};
-	//onError Callback receives a PositionError object
-	var onError = function(error) {
-	    alert('code: '    + error.code    + '\n' +
-	        'message: ' + error.message + '\n');
-	}
-	
-	this.initialize();
+  this.buoyMap = function(event) {
+      navigator.geolocation.getCurrentPosition(onSuccess, onError,{'enableHighAccuracy':true,'timeout':10000});
+  }
+
+  //onSuccess callback receives a Position object
+  var onSuccess = function(position) {
+    
+    var myLat = position.coords.latitude;
+    var myLng = position.coords.longitude;
+    var myLatLng = new google.maps.LatLng(myLat,myLng)
+    
+    //MAP
+    var mapOptions = {
+        center: myLatLng,
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        disableDefaultUI: true
+    };
+    
+    //var navHeight = $('.navbar').height();
+    //$('#mapCanvas').css({ top: navHeight });      
+    
+    var map = new google.maps.Map(document.getElementById("mapCanvas"),mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Me'
+    });
+  };
+  //onError Callback receives a PositionError object
+  var onError = function(error) {
+      alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+  }
+  
+  this.initialize();
 }
 
 MapView.template = Handlebars.compile($('#map-tpl').html());
--- www/js/main.js
+++ www/js/main.js
@@ -1,59 +1,55 @@
 
 var app = {
+  registerEvents: function() {
+    $(window).on('hashchange', $.proxy(this.route, this));
+  },
 
-    registerEvents: function() {
-        $(window).on('hashchange', $.proxy(this.route, this));
-    },
+  route: function() {
+    var self = this;
+    var hash = window.location.hash;
+    if (hash.match(this.homeURL)) {
+      if (this.homePage) {
+        $('body').html(this.homePage.el);
+      }
+      else {
+        this.homePage = new HomeView().render();
+        $('body').html(this.homePage.el);
+      }
+      return;
+    }
+    
+    if (hash.match(this.mapURL)) {
+      if (this.mapPage) {
+        $('body').html(this.mapPage.el);
+        this.mapPage.registerEvents();
+      } 
+      else {
+        this.mapPage = new MapView().render();
+        $('body').html(this.mapPage.el);
+      } 
+      return;
+    }
+  },
 
-    route: function() {
-        var self = this;
-        var hash = window.location.hash;
-        if (hash.match(this.homeURL)) {
-            if (this.homePage) {
-                $('body').html(this.homePage.el);
-                //this.slidePage(this.homePage);
-            }
-            else {
-                this.homePage = new HomeView().render();
-                $('body').html(this.homePage.el);
-                //this.slidePage(this.homePage);
-            }
-            return;
-        }
-        
-        if (hash.match(this.mapURL)) {
-            if (this.mapPage) {
-                $('body').html(this.mapPage.el);
-                //this.slidePage(this.mapPage);
-            } 
-            else {
-                this.mapPage = new MapView().render();
-                $('body').html(this.mapPage.el);
-                //this.slidePage(this.mapPage);
-            } 
-            return;
-        }
-    },
+  initialize: function() {
+    var self = this;
+    this.homeURL = '#home';
+    this.mapURL = '#map';
+    window.location.hash = this.homeURL;
+    this.registerEvents();
+    this.store = new LocalStorageStore(function() {
+        self.route();
+    });
+},
 
-    initialize: function() {
-        var self = this;
-        this.homeURL = '#home';
-        this.mapURL = '#map';
-        window.location.hash = this.homeURL;
-        this.registerEvents();
-        this.store = new LocalStorageStore(function() {
-            self.route();
-        });
-    },
-
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } 
-        else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
+  showAlert: function (message, title) {
+    if (navigator.notification) {
+      navigator.notification.alert(message, null, title, 'OK');
+    } 
+    else {
+      alert(title ? (title + ": " + message) : message);
+    }
+  },
 };
 
 app.initialize();
