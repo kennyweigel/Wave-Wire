383a94c8f0ac39fd84eb05c0267ac6700ff8efc3
Kenny Weigel <kennyweigel@gmail.com>
2013-03-27 07:40:29 -0400

    changed home rendering

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -10,19 +10,30 @@ var HomeView = function(store) {
   }
 
   this.render = function() {
-    this.el.html(HomeView.template());
-    this.renderFavorites();
+    console.log('render');
+    var testt = JSON.parse(window.localStorage.getItem('favorites'));
+    this.el.html(HomeView.template(testt));
+    //this.renderFavorites();
     return this;
   }
 
   this.renderFavorites = function() {
-    store.getFavorites(function(favorites) {
-      $('#favBuoys').html(HomeView.favsTemplate(favorites));
-    });
+    //var testt = store.getFavorites();
+    $('body div #favBuoys').html('<p>Test</p>');
+    console.log($('#favBuoys').html());
+    console.log('render favs');
+    var testt = JSON.parse(window.localStorage.getItem('favorites'));
+    console.log(testt);
+    var testtt = HomeView.favsTemplate(testt);
+    console.log(testtt);
+    //$('#favBuoys').html(HomeViewfavsTemplate(testt));
+    //store.getFavorites(function(favorites) {
+    //  $('#favBuoys').html(HomeView.favsTemplate(favorites));
+    //});
   }
 
   this.initialize();
 }
 
 HomeView.template = Handlebars.compile($("#home-tpl").html());
-HomeView.favsTemplate = Handlebars.compile($("#favs-tpl").html());
+//HomeView.favsTemplate = Handlebars.compile($("#favs-tpl").html());
--- www/js/LocalStorage.js
+++ www/js/LocalStorage.js
@@ -1,14 +1,19 @@
 var LocalStorageStore = function(successCallback, errorCallback) {
 
-  this.getFavorites = function(callback) {
-    var getFavs = JSON.parse(window.localStorage.getItem("favorites"));
-    callLater(callback, getFavs);
+  this.getFavorites = function() {
+    var favss = JSON.parse(window.localStorage.getItem('favorites'));
+    return favss;
   }
+
+  //this.getFavorites = function(callback) {
+  //  var getFavs = JSON.parse(window.localStorage.getItem("favorites"));
+  //  callLater(callback, getFavs);
+  //}
 
   // Used to simulate async calls. This is done to provide a consistent interface with stores (like WebSqlStore)
   // that use async data access APIs
   var callLater = function(callback, data) {
-    if (callback) {
+   if (callback) {
       setTimeout(function() {
         callback(data);
       });
