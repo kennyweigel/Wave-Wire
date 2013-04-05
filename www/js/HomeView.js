96fe61147a59a2111638d41438bc907b7167207c
Kenny Weigel <kennyweigel@gmail.com>
2013-04-05 18:55:27 -0400

    remove extra alerts

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -20,15 +20,14 @@ var HomeView = function(store) {
 
   this.renderFavorites = function() {
     console.log('render favs');
+    alert('render favs');
     $('#favBuoys').html(HomeView.favsTemplate(store.getFavorites()));
   }
   
   this.getTest = function() {
-    alert('getTest');
     var currentIDs = store.getFavorites();
-    alert('curentIDs:'+currentIDs);
+    
     for (var i = 0; i < currentIDs.length; i++) {
-      alert(i);
       updateInit('http://www.ndbc.noaa.gov/mobile/station.php?station='+currentIDs[i].id,currentIDs[i].id);
     }
 
@@ -40,21 +39,15 @@ var HomeView = function(store) {
     }
     
     function update (html, status, EXTRA) {
-      alert('in update');
-      alert(html);
-      alert(status);
       if(status != 'success') {
-        alert('DNE success');
+        alert('GET was unsuccessful');
         return;
       }
       
       locals = store.getFavorites();
-      alert(locals);
       for (var j=0; j < locals.length; j++) {
         if (locals[j].id == EXTRA) {
-          alert('local == id');
           locals[j].data = html;
-          alert('locals new '+locals[j].data);
           store.setFavorites(locals);
           this.renderFavorites();
           alert('rendered');
