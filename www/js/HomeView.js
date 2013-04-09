80056c573a731537851307800d7ae7a6a6d28703
Kenneth Weigel <kennyweigel@gmail.com>
2013-04-09 12:35:44 -0400

    update will apply to all buoys now

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -39,60 +39,33 @@ var HomeView = function(store) {
     for (var i = 0; i < currentIDs.length; i++) {
       updateInit('http://www.ndbc.noaa.gov/mobile/station.php?station='+currentIDs[i].id,currentIDs[i].id);
     }
-  }
-
-  var updateInit = function(url,EXTRA) {
-    $.get(url,function(html,status){
-      update(html,status,EXTRA);
-    });
-
-  }
     
-  //change back to var when deploying to phone  
-  var update = function(html, status, EXTRA) {
-    if(status != 'success') {
-      alert('GET was unsuccessful');
-      return;
+    function updateInit(url,EXTRA) {
+      $.get(url,function(html,status){
+        update(html,status,EXTRA);
+      });
     }
     
-    locals = store.getFavorites();
-    for (var j=0; j < locals.length; j++) {
-      if (locals[j].id == EXTRA) {
-        locals[j].data = app.processBuoyData(html);
-        app.homePage.renderFavorites(locals);
-        break;
+    function update(html,status,EXTRA) {
+      if(status != 'success') {
+        alert('GET was unsuccessful');
+        return;
+      }
+      
+      locals = store.getFavorites();
+      for (var j=0; j < locals.length; j++) {
+        if (locals[j].id == EXTRA) {
+          locals[j].data = app.processBuoyData(html);
+          store.setFavorites(locals);
+          app.homePage.renderFavorites(locals);
+          break;
+        }
       }
     }
   }
-  
+
   this.initialize();
 }
-
-// for testing in PC browser -----------------------------------------------
-/*
-var testHTML = 
-'<?xml version="1.0" encoding="utf-8" ?>\
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">\
-<html xmlns="http://www.w3.org/1999/xhtml">\
-<head>\
-<title>NDBC/44007</title>\
-<link href="ndbc.css" rel="stylesheet" type="text/css" media="all" />\
-</head>\
-<body>\
-<h1><img src="noaa_logo.gif" width="40" height="40" alt="NOAA Logo" class="logo" /> NDBC/44007</h1><p>43.531N 70.144W</p>\
-<h2>Weather Conditions</h2>\
-<p>7:50 am EDT<br />\
-1160 GMT 04/07/13<br />\
-Seas: 2.3 ft<br />\
-Peak Period: 12 sec<br />\
-Pres: 30.31 rising<br />\
-Air Temp: 40.1 &#176;F<br />\
-Water Temp: 40.1 &#176;F<br />\
-</p>\
-<p><a href="/mobile/">Main Page</a><br />Feedback: <a href="mailto:webmaster.ndbc@noaa.gov">webmaster.ndbc@noaa.gov</a></p>\
-</body>\
-</html>'
-*/
 
 HomeView.template = Handlebars.compile($('#home-tpl').html());
 HomeView.favsTemplate = Handlebars.compile($('#favs-tpl').html());
