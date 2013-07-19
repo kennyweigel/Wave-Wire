b7892a0db44c9a6450b93d16833261830565f09f
Kenny Weigel <kennyweigel@gmail.com>
2013-07-18 23:08:11 -0400

    removed fastclick for debugging

--- www/js/main.js
+++ www/js/main.js
@@ -2,9 +2,9 @@ var app = {
   
   registerEvents: function() {
     $(window).on("hashchange", $.proxy(this.route, this));
-    window.addEventListener('load', function() {
-      FastClick.attach(document.body);
-    }, false);
+    //window.addEventListener('load', function() {
+    //  FastClick.attach(document.body);
+    //}, false);
     $(window).on("orientationchange", function(event) {
       event.preventDefault();
     });
