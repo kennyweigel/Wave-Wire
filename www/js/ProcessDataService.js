0990280baa74308c2b1c7d9a7d982cdb69d17924
Kenny Weigel <kennyweigel@gmail.com>
2014-03-24 08:35:57 -0400

    updating the process data service, still working on it

--- www/js/ProcessDataService.js
+++ www/js/ProcessDataService.js
@@ -3,37 +3,59 @@ angular.module("Main")
 .factory("ProcessDataService", function($rootScope) {
 
     var getDate = function(data) {
-            var date = null,
-                time = null;
-
-            date = parseInt(data.month, 10) + "/" + data.day + "/" + data.year;
-            time = parseInt(data.hour, 10) + ":" + data.minute;
-
-            return {
-                "date": date,
-                "time": time
-            };
+            return parseInt(data.month, 10) + "/" + data.day + "/" + data.year;
+        },
+        getTime = function(data) {
+            return parseInt(data.hour, 10) + ":" + data.minute;
         },
         getSpeed = function(input) {
             if ($rootScope.userData.units == "us") {
-                return input * 2.23694;
+                return {
+                    "value": roundTenth(input * 2.23694),
+                    "units": "mph"
+                };
             } else if ($rootScope.userData.units == "metric") {
-                return input * 3.6;
+                return {
+                    "value": roundTenth(input * 3.6),
+                    "units": "kph"
+                };
             }
+        },
+        getDirection = function(input) {
+            //TODO ADD LOGIC HERE
+            return {
+                "value": input,
+                "units": "degrees"
+            };
         },
         getHeight = function(input) {
             if ($rootScope.userData.units == "us") {
-                return input * 3.28084;
+                return {
+                    "value": roundTenth(input * 3.28084),
+                    "units": "ft"
+                };
             } else if ($rootScope.userData.units == "metric") {
-                return input;
+                return {
+                    "value": roundTenth(input),
+                    "units": "m"
+                };
             }
         },
         getTemp = function(input) {
             if ($rootScope.userData.units == "us") {
-                return input * 1.8 + 32;
+                return {
+                    "value": roundTenth(input * 1.8 + 32),
+                    "units": "F"
+                };
             } else if ($rootScope.userData.units == "metric") {
-                return input;
+                return {
+                    "value": roundTenth(input),
+                    "units": "C"
+                };
             }
+        },
+        roundTenth = function(input) {
+            return Math.round(input * 10) / 10;
         },
         getDataRowArray = function(data) {
             var removeNull = function(data) {
@@ -71,49 +93,155 @@ angular.module("Main")
 
             return data;
         },
-        processIndividual = function(data) {
-            var dateTime = null,
-                date = null,
-                time = null;
-                processedData = null,
+        processRow = function(row) {
+            var processedRow = {},
                 key = null;
+
+            processedRow["date"] = {
+                "value": getDate(row),
+                "units": "NA"
+            };
+            processedRow["time"] = {
+                "value": getTime(row),
+                "units": "NA"
+            };
+
+            for (key in row) {
+                if (row[key] != "MM") {
+                    switch (key) {
+                        case "windDirection":
+                            processedRow["windDirection"] = getDirection(row[key]);
+                            processedRow["windDirection"].field = "Wind Direction";
+                            break;
+                        case "windSpeed":
+                            processedRow["windSpeed"] = getSpeed(row[key]);
+                            processedRow["windSpeed"].field = "Wind Speed";
+                            break;
+                        case "windGust":
+                            processedRow["windGust"] = getSpeed(row[key]);
+                            processedRow["windGust"].field = "Wind Gust";
+                            break;
+                        case "waveHeight":
+                            processedRow["waveHeight"] = getHeight(row[key]);
+                            processedRow["waveHeight"].field = "Wave Height";
+                            break;
+                        case "waveDominantPeriod":
+                            processedRow["waveDominantPeriod"] = {
+                                "value": row[key],
+                                "units": "s",
+                                "field": "Wave Dominant Period"
+                            };
+                            break;
+                        case "waveAveragePeriod":
+                            processedRow["waveAveragePeriod"] = {
+                                "value": row[key],
+                                "units": "s",
+                                "field": "Wave Average Period"
+                            };
+                            break;
+                        case "medianWaveDirection":
+                            processedRow["medianWaveDirection"] = getDirection(row[key]);
+                            processedRow["medianWaveDirection"].field = "Wave Direction";
+                            break;
+                        case "pressure":
+                            processedRow["pressure"] = {
+                                "value": row[key],
+                                "units": "hPa",
+                                "field": "Pressure"
+                            };
+                            break;
+                        case "airTemp":
+                            processedRow["airTemp"] = getTemp(row[key]);
+                            processedRow["airTemp"].field = "Air Temp";
+                            break;
+                        case "waterTemp":
+                            processedRow["waterTemp"] = getTemp(row[key]);
+                            processedRow["waterTemp"].field = "Water Temp";
+                            break;
+                        case "dewPoint":
+                            processedRow["dewPoint"] = getTemp(row[key]);
+                            processedRow["dewPoint"].field = "Dew Point";
+                            break;
+                        case "visibilty":
+                            processedRow["visibilty"] = {
+                                "value": row[key],
+                                "units": "nmi",
+                                "field": "Visibility"
+                            };
+                            break;
+                        case "ptdy":
+                            processedRow["ptdy"] = {
+                                "value": row[key],
+                                "units": "hPa",
+                                "field": "PTDY"
+                            };
+                            break;
+                        case "tide":
+                            processedRow["tide"] = {
+                                "value": row[key],
+                                "units": "ft",
+                                "field": "Tide"
+                            };
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    processedRow[key] = {
+                        "value": "NA",
+                        "units": ""
+                    };
+                }
+            }
+
+            return processedRow;
+        },
+        processIndividual = function(data) {
+            var dataArray = [],
+                processedRow = {},
+                keys = [],
+                i = 0;
+                keysLength = 0;
 
             //gets the 3rd row only, the latest data
             data = data.split('\n');
             data = data[2];
             data = getDataRowArray(data);
 
-            dateTime = getDate(data);
-            time = dateTime.time;
-            date = dateTime.date;
+            processedRow = processRow(data);
+            console.log(processedRow);
 
-            for (key in data) {
-                if (data[key] == "MM")
-            }
-            return [
-                "date " +  date,
-                "time " + time,
-                "windDirection " + data.windDirection,
-                "windSpeed " + data.windSpeed,
-                "windGust " + data.windGust,
-                "waveHeight " + data.waveHeight,
-                "waveDominantPeriod " + data.waveDominantPeriod,
-                "waveAveragePeriod " + data.waveAveragePeriod,
-                "medianWaveDirection " + data.medianWaveDirection,
-                "pressure " + data.pressure,
-                "airTemp " + data.airTemp,
-                "waterTemp " + data.waterTemp,
-                "dewPoint " + data.dewPoint,
-                "visibilty " + data.visibilty,
-                "ptdy " + data.ptdy,
-                "tide " + data.tide
+            //need to arrange data in correct order
+            dataArray.push(processedRow.time.value + " " + processedRow.date.value);
+            
+            keys = [
+                "windDirection",
+                "windSpeed",
+                "windGust",
+                "waveHeight",
+                "waveDominantPeriod",
+                "waveAveragePeriod",
+                "medianWaveDirection",
+                "pressure",
+                "airTemp",
+                "waterTemp",
+                "dewPoint",
+                "visibilty",
+                "ptdy",
+                "tide"
             ];
+            keysLength = keys.length;
 
+            for (i = 0; i < keysLength; i++) {
+                if (processedRow[keys[i]]["value"] != "NA") {
+                    dataArray.push(processedRow[keys[i]]);
+                }
+            }
+            return dataArray;
         },
         processFavorites = function(data) {
 
         };
-
 
     return {
         individual: function(data) {
